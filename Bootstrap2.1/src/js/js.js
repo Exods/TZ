fd4599aa02d59c8dc7bde9748921c11c6f789608
Gorshkov Sergey <gorshkovs372@gmail.com>
2020-04-08 15:24:57 +0300

    Bootstrap2.1Fix

--- Bootstrap2.1/src/js/js.js
+++ Bootstrap2.1/src/js/js.js
@@ -1,10 +1,5 @@
 $(function(){
-   console.log(1);
-
-   $("#btn_menu").click(function(){
-      console.log(2);
-      $("#menu_list").toggleClass("open");
+     $(".btn_menu").click(function(){
+      $(".menu_list").toggleClass("open");
    });
-
-
 });
