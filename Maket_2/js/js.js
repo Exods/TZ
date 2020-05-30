f9afff44ff24a0363e2642dfa80ed220da5a99ff
Gorshkov Sergey <gorshkovs372@gmail.com>
2020-05-30 14:40:38 +0300

    Maket_2(welcome-block):add slider

---
+++ Maket_2/js/js.js
@@ -0,0 +1,10 @@
+$(function(){
+  $('.bxslider').bxSlider({
+    mode: 'fade',
+    captions: false,
+    slideWidth: 1250,
+    adaptiveHeight:true,
+    responsive:true,
+    pager:false
+  });
+});
